1265f929db5fdad1f42013db1dac4f33b135d471
rafael01gx <rafael_junio_moraes@hotmail.com>
2026-01-31 00:08:39 -0300

    Adiciona arquivos iniciais SpringBoot.

---
+++ src/main/java/br/com/inspectflow/InspectflowApplication.java
@@ -0,0 +1,13 @@
+package br.com.inspectflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspectflowApplication {
+
+	 static void main(String[] args) {
+		SpringApplication.run(InspectflowApplication.class, args);
+	}
+
+}

e87b9f3947e0c48b1b790a4b84b5f9da94cf4b72
RafaelGx <rafael_junio_moraes@hotmail.com>
2026-04-07 16:07:56 -0300

    Checkpoint

--- src/main/java/br/com/inspectflow/application/stock/services/FindAllByEquipmentIdService.java
+++ src/main/java/br/com/inspectflow/application/stock/services/FindAllByEquipmentIdService.java
@@ -5,7 +5,6 @@ import br.com.inspectflow.application.stock.dto.StockItemResponse;
 import br.com.inspectflow.application.stock.ports.in.FindAllByEquipmentIdUseCase;
 import br.com.inspectflow.domain.equipment.models.Equipment;
 import br.com.inspectflow.domain.equipment.repositories.EquipmentRepository;
-import br.com.inspectflow.domain.stock.repositories.StockItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +14,6 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class FindAllByEquipmentIdService implements FindAllByEquipmentIdUseCase {
-    private final StockItemRepository repository;
     private final EquipmentRepository equipmentRepository;
 
 

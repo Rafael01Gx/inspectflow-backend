cce36a31cc5529f1349900dbc2086faa8dd33c5e
rafael01gx <rafael_junio_moraes@hotmail.com>
2026-05-01 02:49:45 -0300

    config: CORS

--- src/main/java/br/com/inspectflow/infrastructure/config/security/SecurityConfig.java
+++ src/main/java/br/com/inspectflow/infrastructure/config/security/SecurityConfig.java
@@ -83,7 +83,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(Arrays.asList(appHosts.web().replace("https", "http"), appHosts.api().replace("http", "https")));
+        configuration.setAllowedOrigins(Arrays.asList(appHosts.web(),"http://inspectflow.rflgx.com.br","http://localhost:4000"));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST","PATCH","PUT", "DELETE", "OPTIONS"));
 

63db81fee149ebb28f48fd08e838325bf134b88a
rafael01gx <rafael_junio_moraes@hotmail.com>
2026-05-18 02:45:20 -0300

    Based on the changes in the diff, here is a concise commit message:
    Chore: update .gitignore to exclude logs directory
    
    - Add `/logs` to `.gitignore` to prevent log files from being tracked by version control.

--- src/main/java/br/com/inspectflow/application/order/events/WorkOrderAddDocumentEvent.java
+++ src/main/java/br/com/inspectflow/application/order/events/WorkOrderAddDocumentEvent.java
@@ -4,23 +4,30 @@ import br.com.inspectflow.domain.order.enums.OrderAttachmentType;
 import br.com.inspectflow.domain.order.models.OrderAttachment;
 import br.com.inspectflow.domain.order.models.WorkOrder;
 
+import java.util.UUID;
+
 public record WorkOrderAddDocumentEvent(
         String numeroOrdemServico,
         String statusOrdemServico,
         String equipmentName,
+        String equipmentCode,
         OrderAttachmentType tipoDocumento,
         String nomeArquivo,
-        String assigneeEmail
+        String assigneeEmail,
+
+        UUID assigneeId
 ) {
 
     public static  WorkOrderAddDocumentEvent from(WorkOrder workOrder, OrderAttachment orderAttachment) {
         return new WorkOrderAddDocumentEvent(
                 workOrder.getId().toString(),
-                workOrder.getOrderStatus().toString(),
+                workOrder.getOrderStatus().getValue(),
                 workOrder.getEquipmentName(),
+                workOrder.getEquipment().getCode(),
                 orderAttachment.getType(),
                 orderAttachment.getFileName(),
-                workOrder.getAssignee().getEmail()
+                workOrder.getAssignee().getEmail(),
+                workOrder.getAssignee().getId()
         );
     }
 }
--- src/main/java/br/com/inspectflow/application/order/events/listener/WorkOrderAddDocumentListener.java
+++ src/main/java/br/com/inspectflow/application/order/events/listener/WorkOrderAddDocumentListener.java
@@ -2,7 +2,9 @@ package br.com.inspectflow.application.order.events.listener;
 
 import br.com.inspectflow.application.email.dto.SendWorkOrderUpdateMailSend;
 import br.com.inspectflow.application.email.ports.in.SendWorkOrderUpdateMailUseCase;
+import br.com.inspectflow.application.notification.templates.WorkOrderAddDocumentNotification;
 import br.com.inspectflow.application.order.events.WorkOrderAddDocumentEvent;
+import br.com.inspectflow.domain.notification.enums.NotificationType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -14,6 +16,7 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @Slf4j
 public class WorkOrderAddDocumentListener {
     private final SendWorkOrderUpdateMailUseCase sendWorkOrderUpdateMailUseCase;
+    private final WorkOrderAddDocumentNotification workOrderAddDocumentNotification;
 
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
@@ -23,12 +26,14 @@ public class WorkOrderAddDocumentListener {
                     event.numeroOrdemServico(),
                     event.statusOrdemServico(),
                     event.equipmentName(),
+                    event.equipmentCode(),
                     event.tipoDocumento(),
                     event.nomeArquivo(),
                     event.assigneeEmail()
             ));
+            workOrderAddDocumentNotification.execute(event, NotificationType.INFO);
         } catch (Exception ex) {
-            log.error("Falha ao enviar email para WorkOrder id={}: {}", event.numeroOrdemServico(), ex.getMessage(), ex);
+            log.error("Falha ao enviar email ou notificação para WorkOrder id={}: {}", event.numeroOrdemServico(), ex.getMessage(), ex);
         }
     }
 }
